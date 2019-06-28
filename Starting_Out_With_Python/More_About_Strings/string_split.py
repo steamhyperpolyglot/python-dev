0238e29aa6330b939d175333e7b8fc7417844da2
Sherman Chen <sherman@thecxhacker.guru>
2019-06-28 16:03:21 +0800

    Completed Code Samples for Chapter 8

---
+++ Starting_Out_With_Python/More_About_Strings/string_split.py
@@ -0,0 +1,14 @@
+# This program demonstrates the split method.
+
+def main():
+    # Create a string with multiple words.
+    my_string = 'One two three four'
+
+    # Split the string.
+    word_list = my_string.split()
+
+    # Print the list of words.
+    print(word_list)
+
+# Call the main function.
+main()
---
+++ Starting_Out_With_Python/More_About_Strings/validate_password.py
@@ -0,0 +1,15 @@
+import login
+
+def main():
+    # Get a password from the user.
+    password = input('Enter your password: ')
+
+    # Validate the password.
+    while not login.valid_password(password):
+        print('That password is not valid.')
+        password = input('Enter your password: ')
+    
+    print('That is a valid password.')
+
+# Call the main function
+main()
