d859d0c13bacc53d685ea83e461122a240e0ffe7
Sherman Chen <uxdailygeek@gmail.com>
2019-12-30 16:50:33 +0800

    Completed the cell_phone_list.py example

---
+++ Starting_Out_With_Python/Classes_OOP/coin_argument.py
@@ -0,0 +1,27 @@
+# This program passes a Coin object as an argument
+# to a function.
+import coin
+
+
+# main function
+def main():
+	# Create a Coin object
+	my_coin = coin.Coin()
+	
+	# This will display 'Heads'
+	print(my_coin.get_sideup())
+	
+	# Pass the object to the flip function.
+	flip(my_coin)
+	
+	# This might display 'Heads', or it might
+	# display 'Tails'.
+	print(my_coin.get_sideup())
+
+
+def flip(coin_obj):
+	coin_obj.toss()
+
+
+# Call the main function
+main()
