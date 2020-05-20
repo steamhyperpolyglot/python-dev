b04bc82071f0315d78e7c1032f4578067636eb4b
Sherman Chen <uxdailygeek@gmail.com>
2020-05-20 20:50:56 +0800

    Added code examples for Chapter 8

---
+++ Impractical_PyProjects/08_counting_syllabus/test_count_syllables_w_full_corpus.py
@@ -0,0 +1,16 @@
+import sys
+import count_syllables
+
+with open('train.txt') as in_file:
+    words = set(in_file.read().split())
+
+missing = []
+
+for word in words:
+    try:
+        num_syllables = count_syllables.count_syllables(word)
+        print(word, num_syllables, end='\n')
+    except KeyError:
+        missing.append(word)
+
+print("Missing words:", missing, file=sys.stderror)
