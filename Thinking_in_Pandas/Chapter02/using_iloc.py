ebdc456b8ee1a9d345f4d10ca5124db2d51f821d
Sherman Chen <uxdailygeek@gmail.com>
2020-09-17 03:03:32 +0800

    Updated code examples to different books

---
+++ Thinking_in_Pandas/Chapter02/using_iloc.py
@@ -0,0 +1,12 @@
+import pandas as pd
+account_info = pd.DataFrame({
+	"name": ["Bob", "Mary", "Mita"],
+	"account": [123846, 123972, 347209],
+	"balance": [123, 3972, 7209],
+})
+
+print(account_info.iloc[1])
+print()
+print(account_info.iloc[0:2])
+print()
+print(account_info.iloc[:])
--- Thinking_in_Pandas/Chapter02/using_loc.py
+++ Thinking_in_Pandas/Chapter02/using_loc.py
@@ -1,2 +1,12 @@
-# Listing 2-6: Extracting a sub-DataFrame from a multi-indexed multi-level column
-# DataFrame using iloc.
+# Listing 2-7: Example of accessing rows and columns in a DataFrame using loc
+
+import pandas as pd
+account_info = pd.DataFrame({
+	"name": ["Bob", "Mary", "Mita"],
+	"account": [123846, 123972, 347209],
+	"balance": [123, 3972, 7209],
+})
+
+print(account_info.loc[1, "balance"])
+print()
+print(account_info.loc[:, ["name", "balance"]])
