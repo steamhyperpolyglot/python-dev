eede1c2427b00281523c37c7123d3d82cadb5276
Sherman Chen <uxdailygeek@gmail.com>
2021-01-17 22:45:55 +0800

    re-organized the folder structure

---
+++ PyQt5/create_gui_app/basic_features/ui_dialogs/ui_qn_messagebox.py
@@ -0,0 +1,30 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, \
+    QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+
+		self.setWindowTitle("My App")
+
+		button = QPushButton("Press me for a dialog!")
+		button.clicked.connect(self.button_clicked)
+		self.setCentralWidget(button)
+
+	def button_clicked(self, s):
+		button = QMessageBox.question(self, "Question dialog", 
+			"The longer message")
+
+		if button == QMessageBox.Yes:
+			print("Yes!")
+		else:
+			print("No!")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec_()
