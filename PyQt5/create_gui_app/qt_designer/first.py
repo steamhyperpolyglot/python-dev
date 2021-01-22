0bbbe2098363502d029974311a4b6f71a5caaff8
Sherman Chen <uxdailygeek@gmail.com>
2021-01-23 01:06:31 +0800

    Finished code examples for the Designer chapter

---
+++ PyQt5/create_gui_app/qt_designer/first.py
@@ -0,0 +1,7 @@
+from MainWindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
